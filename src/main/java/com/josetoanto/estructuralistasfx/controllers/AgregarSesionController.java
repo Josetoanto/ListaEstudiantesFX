88c81da7b4fba47df87a137906ecbda3f6d8d848
Josetoanto <jarf1369@gmail.com>
2024-03-13 19:36:13 -0600

    Agregando interfaz principal y agregar

--- src/main/java/com/josetoanto/estructuralistasfx/controllers/AgregarSesionController.java
+++ src/main/java/com/josetoanto/estructuralistasfx/controllers/AgregarSesionController.java
@@ -1,4 +1,20 @@
 package com.josetoanto.estructuralistasfx.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class AgregarSesionController {
+
+    @FXML
+    private TextField contraseñaSesion;
+
+    @FXML
+    private TextField nombreSesion;
+
+    @FXML
+    void onAgregarClick(MouseEvent event) {
+
+    }
+
 }

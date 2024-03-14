65fe82b4ff9f7f94b6e06651a4c04a78cff7e784
MarcosBrindis <markos_brind95@hotmail.com>
2024-03-13 22:52:13 -0600

    agregar estudiante

--- src/main/java/com/josetoanto/estructuralistasfx/controllers/AgregarAlumnoController.java
+++ src/main/java/com/josetoanto/estructuralistasfx/controllers/AgregarAlumnoController.java
@@ -1,13 +1,15 @@
 package com.josetoanto.estructuralistasfx.controllers;
 
-<<<<<<< HEAD
 import com.josetoanto.estructuralistasfx.models.Estudiante;
-=======
+
 import com.josetoanto.estructuralistasfx.models.ListaAsistencia;
->>>>>>> menuProfesorLogica
+
 import javafx.fxml.FXML;
+
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+
 
 public class AgregarAlumnoController {
 
@@ -24,9 +26,52 @@ public class AgregarAlumnoController {
 
     @FXML
     void onAgregarClick(MouseEvent event) {
+        boolean Encontrada=false;
+        int age = Integer.parseInt(txt_fechaIngreso.getText());
+        age=age%100;
+        Estudiante estudiante=new Estudiante();
+        String matricula=estudiante.generarNumeroMatricula(age);
+        if (listaAsistencia.getListaEstudiantes().isEmpty()){
+            estudiante=new Estudiante(Integer.parseInt(txt_fechaIngreso.getText()),matricula,txt_name.getText(),txt_lastName.getText(),estudiante.getMaterias());
+            listaAsistencia.addEstudiante(estudiante);
+            alertaAgregadoCorrectamente(matricula);
+        }
+        else {
+            for (Estudiante matriculaExiste : listaAsistencia.getListaEstudiantes()) {
+                if (matriculaExiste.getMatricula().equals(matricula)) {
+                    Encontrada = true;
+                    System.out.println("se agrego correctamente");
+                }
+            }
+            if (Encontrada){
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("matricula ya existe, por lo tanto no se agregara el alumno.");
+                alert.showAndWait();
+            }
+            else {
+                estudiante=new Estudiante(Integer.parseInt(txt_fechaIngreso.getText()),matricula,txt_name.getText(),txt_lastName.getText(),estudiante.getMaterias());
+                listaAsistencia.addEstudiante(estudiante);
+                alertaAgregadoCorrectamente(matricula);
+
+            }
+
+
+
+        }
     }
 
     public void setListaAsistencia(ListaAsistencia listaAsistencia) {
         this.listaAsistencia = listaAsistencia;
+    }
+
+    public void alertaAgregadoCorrectamente(String matricula){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("agregado Exitosamente");
+        alert.setHeaderText(null);
+        alert.setContentText("se agrego correctamente el alumno\n su matricula es: "+ matricula);
+        alert.showAndWait();
+        setListaAsistencia(listaAsistencia);
     }
 }
--- src/main/java/com/josetoanto/estructuralistasfx/controllers/BuscarAlumnoController.java
+++ src/main/java/com/josetoanto/estructuralistasfx/controllers/BuscarAlumnoController.java
@@ -22,6 +22,7 @@ public class BuscarAlumnoController {
 
     @FXML
     void onSalirClick(MouseEvent event) {
+        
 
     }
 
